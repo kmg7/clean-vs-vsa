af32f364ff162c695603dae247289a1d536a89b7
kmg7 <kmlgkcy.dev@gmail.com>
2024-09-06 09:24:25 +0300

    feat(clna:domain): custom error poco, error codes and messages

---
+++ clna/src/core/DPoll.Domain/Common/Errors/Messages.cs
@@ -0,0 +1,9 @@
+﻿namespace DPoll.Domain.Common.Errors
+{
+    public static class ErrorMessages
+    {
+        public const string NotFound = "entity was not found";
+        //public const string InvalidOperation = "operation was not valid";
+        //public const string InvalidInput = "input was not valid";
+    }
+}
