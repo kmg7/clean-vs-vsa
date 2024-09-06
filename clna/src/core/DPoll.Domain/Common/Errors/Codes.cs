b8c55d4dcfd99aac3587ba25cd4aca8b288178a8
kmg7 <kmlgkcy.dev@gmail.com>
2024-09-06 09:24:25 +0300

    feat(clna:domain): custom error poco, error codes and messages

---
+++ clna/src/core/DPoll.Domain/Common/Errors/Codes.cs
@@ -0,0 +1,9 @@
+﻿namespace DPoll.Domain.Common.Errors
+{
+    public static class ErrorCodes
+    {
+        public const string NotFound = "NotFound";
+        //public const string InvalidOperation = "InvalidOperation";
+        //public const string InvalidInput = "InvalidInput";
+    }
+}
---
+++ clna/src/core/DPoll.Domain/Common/Errors/Error.cs
@@ -0,0 +1,22 @@
+﻿using Dpoll.Domain.Common.Enums;
+
+namespace DPoll.Domain.Common.Errors
+{
+    public sealed class Error
+    {
+        public string Code { get; }
+        public string Description { get; }
+        public Error(string code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+
+        public static readonly Error None = new Error(string.Empty, string.Empty);
+
+        public static Error NotFound(EntityType entityType)
+        {
+            return new Error(ErrorCodes.NotFound, $"{entityType} {ErrorMessages.NotFound}");
+        }
+    }
+}
